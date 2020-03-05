68f764cdde263204691c7f1a74faa7b24e24f6a3
katharine permenter <katharinepermenter@katharines-MacBook-Pro.local>
2020-03-05 17:15:30 -0600

    login

--- README.md
+++
@@ -1,9 +0,0 @@
-ArcGIS JS API
-
-Bootstrap
-
-Calcite React
-
-Redux 
-
-Redux-Saga
--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-const port = process.env.PORT || '8080';
+const port = process.env.PORT || '8000';
 app.set('port', port);
 
 const server = http.createServer(app);
